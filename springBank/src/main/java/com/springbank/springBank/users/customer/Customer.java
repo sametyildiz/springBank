806f25d7b98456ec3ae8ae20c42162fffa9cae49
Samet YILDIZ <sametyildiz03@gmail.com>
2022-04-18 01:12:55 +0300

    Account page and list

--- springBank/src/main/java/com/springbank/springBank/users/customer/Customer.java
+++ springBank/src/main/java/com/springbank/springBank/users/customer/Customer.java
@@ -1,5 +1,6 @@
 package com.springbank.springBank.users.customer;
 
+import com.springbank.springBank.users.customer.address.Address;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,7 +21,7 @@ public class Customer {
     private Long ID;
 
     @Column(nullable = false)
-    private String TC;
+    private String national_ID;
 
     @Column(nullable = false)
     private String name;
@@ -29,11 +30,17 @@ public class Customer {
     private String surname;
 
     @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String phone;
 
-    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
     private Set<Account> bankAccount;
 
 
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id",referencedColumnName = "ID")
+    private Address address;
 
 }
